cfd4df5fb5005c1d6587fadad956754613a37b7c
Jailson Santana <52072288+JailsonKbT@users.noreply.github.com>
2026-03-29 21:18:29 -0300

    One-Dimensional Harmonic Oscillator.
    One-Dimensional Harmonic Oscillator.

--- src/Harmonic Oscillator/Velocity_Verlet.py
+++ src/Harmonic Oscillator/Velocity_Verlet.py
@@ -17,7 +17,6 @@
 ||                                                                                        ||
 ========================================================================================'''
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
